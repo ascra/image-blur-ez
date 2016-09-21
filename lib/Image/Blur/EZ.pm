4782a22146aa9d60d8f19dec620d80eb4485e40c
ascra <francis.woolridge@yahoo.com>
2016-09-21 04:05:58 +0000

    stub

---
+++ lib/Image/Blur/EZ.pm
@@ -0,0 +1,18 @@
+package Image::Blur::EZ;
+
+use strict;
+use warnings;
+use Image::Info qw/image_info/; 
+use Imager;
+use Carp qw/croak/;
+
+sub blur {
+}
+
+sub blur_fast {
+}
+
+sub _image_info {
+}
+
+1;
